0dbb429712bd819d2e70c34fb62e3c2611bdfca7
tgwhite <taylorgentrywhite@gmail.com>
2020-06-17 09:29:23 -0700

    cleaning up data folder, cleaning up script to stack elections since 96, santa monica competitiveness

--- Projects/Local government elections, finance/scripts/get_local_CA_elections.r
+++ Projects/Local government elections, finance/scripts/get_local_CA_elections.r
@@ -1,6 +1,17 @@
 library(tidyverse)
 library(rvest)
 library(readxl)
+library(data.table)
+library(scales)
+library(viridisLite)
+
+# get the competitiveness of various types of races
+# figure out the winning vote margin / population for various types of races (over time)
+# take a look at los angeles and santa monica data: how many votes does it take to win....
+# how many people decide an election in??? This works for congress too
+# how many multiple runners are there? can we see people moving up the food chain
+
+# https://csus-dspace.calstate.edu/bitstream/handle/10211.3/210187/Codebook.pdf?sequence=119
 
 setwd("~/Public_Policy/Projects/Local government elections, finance")
 download_election_files = F
@@ -29,6 +40,18 @@ election_files_read_in = map(excel_files, function(the_filename){
   candidates_sheet = candidates_sheet[str_detect(candidates_sheet %>% tolower(), 'cand')]
   file_imported = read_excel(full_out_filename, candidates_sheet)
   names(file_imported) = str_replace(names(file_imported), '#', '_num') %>% tolower()
+  file_imported 
+  
+  char_cols = c('totalwritein_votes', 'writein', 'totvotes')
+  
+  for (col in char_cols) {
+    if ('character' %in% class(file_imported[[col]])) {
+      file_imported[[col]][file_imported[[col]] == '#NULL!'] = NA
+      file_imported[[col]] = as.numeric(file_imported[[col]])
+    }
+  }
+  
+  
   file_imported
 })
 names(election_files_read_in) = excel_files %>% basename() %>% str_remove('[//?]sequence.*') %>% str_remove_all('%20')
@@ -44,9 +67,13 @@ names(election_files_read_in)
 
 # newelected: Multi-county outcome for candidate – 1=Elected to office; 2=Not elected to office; 3=Runoff 
 
-all_files_stacked = election_files_read_in[1:7] %>% bind_rows() %>%
+last_valid_file = 'CEDA1996Data.xls'
+last_file_index = which(names(election_files_read_in) == last_valid_file)
+
+all_files_stacked = election_files_read_in[1:last_file_index] %>% bind_rows() %>%
   mutate(
-    race_unique = paste(raceid, year),
+    date = as.Date(date),
+    race_unique = paste(year, raceid),
     judge = office %>% tolower() %>% str_detect('judge'),
     attorney = office %>% tolower() %>% str_detect('attorney|prosecutor'),
     mayor = office %>% tolower() %>% str_detect('mayor'),
@@ -62,34 +89,88 @@ all_files_stacked = election_files_read_in[1:7] %>% bind_rows() %>%
   filter(
     !office_type_fin %in% paste0(3:6) & !is.na(office_type_fin),
     newelected != 3 # remove runoffs, about 3% of sample
+  ) %>% 
+  arrange(
+    race_unique, -indivtotal_votes
   )
 
-stats_by_race = group_by(all_files_stacked, race_unique, office_type_fin, year) %>%
-  summarize(
-    candidates_per_office = cand_num[1] / vote_num[1],
-    n_incumb = length(incumbent_fin[incumbent_fin == 'Y']),
-    n_incumb_per_office = n_incumb / vote_num[1],
-    n_open_seats = vote_num[1] - n_incumb
-  ) %>%
-  group_by(
-    office_type_fin
-  ) %>%
-  summarize(
-    mean_cand_per_office = mean(candidates_per_office, na.rm = T),
-    median_cand_per_office = median(candidates_per_office, na.rm = T),
-    mean_open_seats = mean(n_open_seats, na.rm = T),
-    mean_n_incumb_per_office = mean(n_incumb_per_office, na.rm = T)
-  ) %>%
-  ungroup() %>%
-  arrange(-mean_cand_per_office) %>%
-  mutate(office_type_fact = factor(office_type_fin, levels= office_type_fin))
+## for each race, get the margin of victory between the lowest winner and highest loser
 
-ggplot(stats_by_race, aes(office_type_fact, mean_cand_per_office, fill = office_type_fin)) +
-  geom_bar(stat = 'identity',  position = 'dodge', show.legend = F) +
-  geom_text(aes(label = mean_cand_per_office %>% round(1)))
 
+all_files_stacked_dt = data.table(all_files_stacked)
+all_files_stacked_dt$area
+
+## get the number of candidates for a race and keep the top N winners
+## find the next lowest
+
+
+stats_by_race = all_files_stacked_dt[, {
+  n_to_vote_for = vote_num[1]
+  total_votes = multitotal_votes[1]
   
-# get stats by date_r
+  n_incumb = length(incumbent_fin[incumbent_fin == 'Y'])
+  n_incumb_per_office = n_incumb / vote_num[1]
+  n_open_seats = vote_num[1] - n_incumb
+  
+  top_n_winners = indivtotal_votes[1:n_to_vote_for]
+  top_loser = indivtotal_votes[(n_to_vote_for+1)]
+  
+  lowest_winner = min(top_n_winners)
+  loser_margin = lowest_winner - top_loser
+  loser_margin_pct = loser_margin / total_votes
+  
+  list(
+    office_type_fin = office_type_fin[1],
+    place = place[1],
+    area = area[1],
+    term = term[1],
+    cntyname = cntyname[1],
+    date = date[1],
+    office = office[1],
+    recode_offname = recode_offname[1],
+    total_votes = total_votes, 
+    n_to_vote_for = n_to_vote_for,
+    n_candidates = cand_num[1],
+    n_incumb = n_incumb,
+    n_incumb_per_office = n_incumb_per_office,
+    n_candidates_per_office = cand_num[1] / n_to_vote_for,
+    n_open_seats = n_open_seats,
+    avg_winner = mean(top_n_winners),
+    sd_winner = sd(top_n_winners),
+    median_winner = median(top_n_winners), 
+    lowest_winner = lowest_winner,
+    top_loser = top_loser,
+    loser_margin = loser_margin,
+    loser_margin_pct = loser_margin_pct
+  )
+  
+}, by = list(race_unique)]
 
-head(all_files_stacked)
-all_files_stacked$incumb %>% table()
+santa_monica_elections = filter(stats_by_race, str_detect(place %>% tolower(), 'monica'))
+
+
+santa_monica_elections$loser_margin
+ggplot(santa_monica_elections, aes(race_unique, loser_margin_pct, fill = n_candidates_per_office)) +
+  geom_bar(stat = 'identity') +
+  # geom_point() +
+  # geom_pointrange(aes(ymin = 0, ymax = loser_margin_pct)) +
+  geom_text(aes(label = comma(loser_margin)), vjust = -0.5) +
+  scale_y_continuous(labels = percent) +
+  labs(
+    y = 'Minimum Winning Margin (%)\nHigher values are less competitive', x = '',
+    title = 'Competitiveness of Santa Monica City Council Elections, 1996-2018',
+    subtitle = 'Minimum winning margin is the difference in votes between the winning candidate with the least votes and losing candidate with the most votes as a percentage of all votes cast.' %>%
+      str_wrap(120),
+    caption = 'Chart: Taylor G. White\nData: California Elections Data Archive (CEDA)'
+  ) +
+  scale_x_discrete(labels = with(santa_monica_elections, paste(year(date), term, 'Term'))) +
+  scale_fill_viridis_c(name = 'Candidates Per\nOpen Seat', option = 'B') +
+  # scale_x_date(breaks = unique(santa_monica_elections$date), date_labels = '%b-%y') +
+  theme(
+    legend.position = 'right',
+    axis.text.x = element_text(angle = 45),
+    plot.subtitle = element_text(face = 'italic'),
+    plot.caption = element_text(face = 'italic', hjust = 0)
+  )
+ggsave('output/santa_monica_council_competitiveness.png', height = 8, width = 10, units = 'in', dpi = 800)
+   
